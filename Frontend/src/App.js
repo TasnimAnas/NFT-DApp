4d46833bd0abb99d5850fbd1f7c044eb23747ea7
Saptarshee Mitra <saptarshee08@gmail.com>
2022-11-05 14:47:04 +0530

    added frontend files

---
+++ Frontend/src/App.js
@@ -0,0 +1,30 @@
+import {BrowserRouter as Router, Route,Routes} from "react-router-dom";
+import { React, useState } from "react";
+import Home from "./components/Home";
+import SellerMain from "./components/SellerMain";
+import SellerLogin from "./components/SellerLogin";
+import CustomerLogin from "./components/CustomerLogin";
+import CustomerMain from "./components/CustomerMain";
+import NoNft from './components/NoNft';
+
+
+function App() {
+  return (
+
+    <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="seller/login" element={<SellerLogin />} />
+          <Route path="seller/dashboard" element={<SellerMain />} />
+          <Route path="customer/login" element={<CustomerLogin />} />
+          <Route path="customer/dashboard" element={<CustomerMain />} />
+          <Route path="customer/dashboard/no-nft" element={<NoNft />} />
+         </Routes>
+       </Router>
+    // <Home/>
+    // <SellerMain/>
+    // <SellerLogin/>
+  );
+}
+
+export default App;
